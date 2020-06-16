3b1d117dd3076f1ac670e589cccadcc7493da41a
andredavid1 <andredavid1@yahoo.com.br>
2020-06-16 10:17:42 -0300

    basic config backend

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3333, () => {
+    console.log("Servidor rodando na porta 3333");
+});
