3d484049fe36bd930e936d0395a4003d437eee81
andredavid1 <andredavid1@yahoo.com.br>
2020-06-17 16:08:40 -0300

    Front-end mapa integrado create-point

--- front-end/src/pages/CreatePoint/index.tsx
+++ front-end/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
-import { FiArrowLeft } from 'react-icons/fi';
 
 const CreatePoint = () => {
     return (
@@ -61,6 +62,15 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+                    
+                    <Map center={[-16.7310258, -49.2942842]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-16.7310258, -49.2942842]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
