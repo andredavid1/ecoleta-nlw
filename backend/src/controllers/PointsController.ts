3888b67945666b8d18562d2aa09cdc6d5ec89b34
andredavid1 <andredavid1@yahoo.com.br>
2020-06-17 01:06:41 -0300

    backend route listar point específico

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -1,7 +1,26 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+    async show(req: Request, res: Response) {
+        const { id } = req.params;
+
+        const point = await knex('points')
+            .where('id', id)
+            .first();
+
+        if(!point) {
+            return res.status(400).json({ message: 'Point not found.'});
+        }
+
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.title');
+
+        return res.json({ point, items });
+    }
+
     async create(req: Request, res: Response) {
         const {
             name,
@@ -44,7 +63,7 @@ class PointsController {
             id: point_id,
             ...point,
         });
-    };
-};
+    }
+}
 
 export default PointsController;
